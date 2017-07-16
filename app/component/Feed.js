ad030a8df580a046bf2dfaae60050c54769fdb15
Tomas Milar <milar@strands.com>
2017-07-16 05:48:35 -0300

    Implement display of News in Feed component + sample news data

--- app/component/Feed.js
+++ app/component/Feed.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {List, ListItem} from 'react-native-elements';
+import {Avatar, Button, Card, Icon, List, ListItem} from 'react-native-elements';
+import {Image, Text, View, StyleSheet} from "react-native";
 
 class Feed extends Component {
   state = {
@@ -30,6 +31,41 @@ class Feed extends Component {
         ))}
       </List>
     );
+
+    const newsItems = this.state.news.map((n, i) => {
+      return (
+        <Card key={i} containerStyle={{padding: 5}}>
+          <View style={{
+            flexDirection: 'row',
+            height: 60,
+          }}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}>
+              <Icon name={n.icon} size={50} color='#43484d'/>
+            </View>
+            <View style={{flex: 3, padding: 5}}>
+              <Text style={{fontSize: 16}}>
+                { !!n.message.started_by.length &&
+                <Text style={{fontWeight: 'bold'}}>{n.message.started_by + " "}</Text>}
+                {n.message.action}
+              </Text>
+            </View>
+          </View>
+        </Card>
+      )
+    });
+
+    const newsListView = (
+      <View>
+        {newsItems}
+      </View>
+    );
+
+    return newsListView;
   }
 }
 
--- app/config/data.js
+++ app/config/data.js
@@ -783,31 +783,34 @@ export const users = [
 export const news = [
   {
     "type": "search", // permanent (until self-resolution?)
-    "active": true,
-    "details": {
-      "started_by": "Papa"
+    "icon": "account-circle",
+    "message": {
+      "started_by": "Papa",
+      "action": "quiere encontrarte."
+    }
+  },
+  {
+    "type": "search", // permanent (until self-resolution?)
+    "icon": "help",
+    "message": {
+      "started_by": "",
+      "action": "Busqueda en curso por ti."
     }
   },
   {
     "type": "new_friend", // transient (until resolved: dismiss/accept/deny)
-    "active": true,
-    "details": {
-      "username": "JuanPerez12",
-      "name": {
-        "first": "Juan",
-        "last": "Perez"
-      }
+    "icon": "account-circle",
+    "message": {
+      "started_by": "Juan Perez",
+      "action": "quiere agregarte."
     }
   },
   {
     "type": "find", // transient
-    "active": true,
-    "details": {
-      "username": "JuanPerez12",
-      "name": {
-        "first": "Juan",
-        "last": "Perez"
-      }
+    "icon": "help",
+    "message": {
+      "started_by": "Encuentrame",
+      "action": "has visto a esta persona?"
     }
   }
 ];
